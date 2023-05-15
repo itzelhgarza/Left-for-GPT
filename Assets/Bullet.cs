a50f7bdec275eec6a24ed7f1e70968f4ba9d8a61
itzelhgarza <itzel.garza216628@potros.itson.edu.mx>
2023-05-14 18:44:25 -0700

    Problemas con el score
    prueba

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+using UnityEngine.UI;
+//using ScoreManager;
+
 public class Bullet : MonoBehaviour
 {
+    public ScoreManager sm;
+    
     public float life = 3;
- 
+    private int puntos;
+
     void Awake()
     {
         Destroy(gameObject, life);
     }
- 
+
     void OnCollisionEnter(Collision collision)
     {
+
+        Debug.Log("buenas");
+        if(collision.gameObject.CompareTag("Enemy")){
+            sm.score++;
+            Debug.Log("Holaaaa");
+            //txtPuntos.GetComponent<Text>().text=puntos.ToString();
+        }else{
+            Debug.Log("woa pasar");
+        }
         //Destroy(collision.gameObject);
         Destroy(gameObject);
+
     }
 }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score;
+    private int displayScore;
+    public Text scoreUI;
+
+    void Start()
+    {
+        score = 0;
+        displayScore = 0;
+    }
+
+    void Update()
+    {
+        if(score != displayScore){
+            displayScore = score;
+            scoreUI.text = displayScore.ToString();
+        }
+    }
+}
