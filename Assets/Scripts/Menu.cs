d0bfd5b9c093b3edb7925032d39dffe36956f327
itzelhgarza <itzel.garza216628@potros.itson.edu.mx>
2023-05-18 21:57:10 -0700

    Pausa, nuevo juego y cursor
    - Al pausar el juego la partida se congela
    - Al morir te aparece la pantalla de game over
    - El cursor ya no aparece por fuera del juego

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -11,8 +11,7 @@ public class menu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       Cursor.lockState = CursorLockMode.Confined; 
- 
+        Cursor.lockState = CursorLockMode.Confined; 
     }
 
     // Update is called once per frame
--- Assets/canvas.cs
+++ Assets/canvas.cs
@@ -5,16 +5,24 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class canvas : MonoBehaviour
 {
- 
+    BarraDeVida playerVida;
+
     private bool escapePress;
     public GameObject menuObject;
     public GameObject menuCrosshair;
+    public GameObject menuNewGame;
     // Start is called before the first frame update
     void Start()
     {
-       Cursor.lockState = CursorLockMode.Confined; 
-       menuObject.SetActive(false);
-       escapePress=true;
+        Cursor.lockState = CursorLockMode.Locked; 
+        menuObject.SetActive(false);
+        menuNewGame.SetActive(false);
+        escapePress=true;
+        Time.timeScale = 1;
+        playerVida = GameObject.FindWithTag("Player").GetComponent<BarraDeVida>();
+        Cursor.visible = false;
+        
+        
     }
 
     // Update is called once per frame
@@ -25,17 +33,33 @@ public class canvas : MonoBehaviour
             if(escapePress){
                 showMenu();
                 hideCross();
+                hideNewGame();
                 Cursor.lockState = CursorLockMode.None;
                 escapePress=false;
+                Time.timeScale = 0;
             }else{
                 hideMenu();
+                hideNewGame();
                 showCross();
-                Cursor.lockState = CursorLockMode.Confined; 
+                Cursor.lockState = CursorLockMode.Locked; 
                 escapePress=true;
+                Time.timeScale = 1;
             }
-             
-             
         }
+        if(playerVida.vida <= 0){
+                showNewGame();
+                hideMenu();
+                hideCross();
+                Cursor.lockState = CursorLockMode.None;
+                Time.timeScale = 0;
+            }
+    }
+
+    public void showNewGame(){
+        menuNewGame.SetActive(true);
+    }
+    public void hideNewGame(){
+        menuNewGame.SetActive(false);
     }
 
     public void showMenu(){
