bdf6355bde090fa9850ee4f538c026630c3c0c43
Melissa-Arreola <78457370+Melissa-Arreola@users.noreply.github.com>
2023-05-19 03:25:35 -0700

    Boton information

--- Library/Bee/artifacts/emscripten/d3ntt/wrapper_emcc.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-"C:\Program Files\Unity Hub\2021.3.17f1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emcc.bat" %* < nul
-exit %ERRORLEVEL%
--- Library/Bee/artifacts/emscripten/yefrj/wrapper_emar.bat
+++
@@ -1,4 +0,0 @@
-
-@echo off
-"C:\Program Files\Unity Hub\2021.3.17f1\Editor\Data\PlaybackEngines\WebGLSupport\BuildTools\Emscripten\emscripten\emar.bat" %* < nul
-exit %ERRORLEVEL%
